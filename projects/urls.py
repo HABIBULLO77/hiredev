fa25103a9a5fe222b66f5cc15c285cad08f80eb0
HABIBULLO77 <h.hamidov77777@gmail.com>
2022-04-19 06:30:50 +0900

    templates

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns =[
     path('projects/', views.projects, name="projects"),
-    path('project/<str:pk>', views.project, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT')
+    return render(request, 'single-project.html')
 
